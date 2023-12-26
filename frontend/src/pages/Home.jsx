d0f7f857be9fc68936dc0beaae5b5e7ff51e1260
KaranGulve4342 <karangulve4342@gmail.com>
2023-12-26 22:43:38 +0530

    Files added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/home.css';
 
-import { container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import heroImg from '../assets/images/hero-img01.jpg';
 import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
+import Subtitle from './../shared/Subtitle';
+import worldImg from '../assets/images/world.png';
+import SearchBar from '../shared/SearchBar';
 
 const Home = () => {
   return (
@@ -27,6 +30,37 @@ const Home = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
                 </p>
               </div>
+            </Col>
+
+            <Col lg='2'>
+              <div className='hero__img-box'>
+                <img src={heroImg} alt=''/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-4'>
+                <img src={heroVideo} alt=''/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-5'>
+                <img src={heroImg02} alt=''/>
+              </div>
+            </Col>
+
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+
+      {/* Hero section start */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3'>
+              <h5 className='services__subtitle'>What we serve</h5>
+              <h2 className='services__title'>We offer our best services</h2>
             </Col>
           </Row>
         </Container>
---
+++ frontend/src/services/ServiceCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./service-card.css"
+
+const ServiceCard = ({item}) => {
+  return (
+    <div>ServiceCard</div>
+  )
+}
+
+export default ServiceCard
