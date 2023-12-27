2e6b2d47d0c8e0c9c6c1e2a28c7bbef87654b90c
KaranGulve4342 <karangulve4342@gmail.com>
2023-12-27 15:34:40 +0530

    Commited changes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './footer.css'
+import {Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import logo from "../../assets/images/logo.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <Container>
+        <Row>
+          <Col lg="3">
+            
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -5,22 +5,81 @@ import ava02 from '../../assets/images/ava-2.jpg'
 import ava03 from '../../assets/images/ava-3.jpg'
 
 const Testimonials = () => {
-  return (
-    <Slider>
-        <div className='testimonial py-4 px-3'>
-            <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
-            </p>
 
-            <div className='d-flex align-items-center gap-4 mt-3'>
-                <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
-                <div>
-                    
+    const settings = {
+        dots: true,
+        infinite:true,
+        autoplay:true,
+        speed:1000,
+        swipeToSlide:true,
+        autoplaySpeed:2000,
+        slideToShow:3,
+
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    slideToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slideToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ]
+    }
+
+    return (
+        <Slider {... settings}>
+            <div className='testimonial py-4 px-3'>
+                <p>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+                </p>
+
+                <div className='d-flex align-items-center gap-4 mt-3'>
+                    <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
+                    <div>
+                        <h6 className='mb-0 mt-3'>John Doe</h6>
+                        <p>Customer</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </Slider>
-  )
+
+            <div className='testimonial py-4 px-3'>
+                <p>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+                </p>
+
+                <div className='d-flex align-items-center gap-4 mt-3'>
+                    <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
+                    <div>
+                        <h6 className='mb-0 mt-3'>Lia Franklin</h6>
+                        <p>Customer</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='testimonial py-4 px-3'>
+                <p>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+                </p>
+
+                <div className='d-flex align-items-center gap-4 mt-3'>
+                    <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
+                    <div>
+                        <h6 className='mb-0 mt-3'>Roman Reighs</h6>
+                        <p>Customer</p>
+                    </div>
+                </div>
+            </div>
+        </Slider>
+    )
 }
 
 export default Testimonials
