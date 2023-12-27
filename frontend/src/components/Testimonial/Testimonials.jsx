e337b71a2ee79b1de52f9b18284e1ade9d9bf505
KaranGulve4342 <karangulve4342@gmail.com>
2023-12-27 14:34:37 +0530

    Files commited

---
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Slider from 'react-slick';
+import ava01 from '../../assets/images/ava-1.jpg'
+import ava02 from '../../assets/images/ava-2.jpg'
+import ava03 from '../../assets/images/ava-3.jpg'
+
+const Testimonials = () => {
+  return (
+    <Slider>
+        <div className='testimonial py-4 px-3'>
+            <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+            </p>
+
+            <div className='d-flex align-items-center gap-4 mt-3'>
+                <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </Slider>
+  )
+}
+
+export default Testimonials
--- frontend/src/services/ServiceCard.jsx
+++ frontend/src/services/ServiceCard.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import "./service-card.css"
 
 const ServiceCard = ({item}) => {
-  return (
-    <div>ServiceCard</div>
-  )
+
+    const {imgUrl, title, desc} = item;
+
+    return (
+        <div className='service__item'>
+            <div className='service__img'>
+                <img src={imgUrl} alt='' />
+            </div>
+            <h5>{title}</h5>
+            <p>{desc}</p>
+        </div>
+    )
 }
 
 export default ServiceCard
