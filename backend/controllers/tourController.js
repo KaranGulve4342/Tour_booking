788c8ad04a3330e13236a43a096a76dc259a866b
KaranGulve4342 <karangulve4342@gmail.com>
2023-12-28 19:31:52 +0530

    Update tourController.js

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -3,24 +3,38 @@ import Tour from '../models/Tour.js';
 // create new tour
 
 export const createTour = async (req, res)=>{
-    const newTour = new Tour(req.body);
+    // const newTour = new Tour(req.body);
 
-    try{
-        const savedTour = await newTour.save();
+    // try{
+    //     const savedTour = await newTour.save();
 
-        res.status(200).json({success:true, message:'Successfully created', data:savedTour})
-    }catch(err){
-        res.status(500).json({success:false, message:'Failed to create. Try again'})
-    }
+    //     res.status(200).json({success:true, message:'Successfully created', data:savedTour})
+    // }catch(err){
+    //     res.status(500).json({success:false, message:'Failed to create. Try again'})
+    // }
 };
 
 // update tour
 export const updateTour = async (req, res) => {
-    try{
+    // const id = req.params.id
+    // try{
+    //     const updatedTour = await Tour.findByIdAndUpdate(id, {
+    //         $set: req.body
+    //     }, {new:true})
 
-    }catch (err){
+    //     res.status(200).json({
+    //         success: true,
+    //         message: "Successfully updated",
+    //         data:updatedTour,
+    //     });
 
-    }
+    // }catch (err){
+    //     res.status(500).json({
+    //         success: false,
+    //         message: "Failed to update",
+    //         data:updatedTour,
+    //     });
+    // }
 };
 // deleteTour
 export const deleteTour = async (req, res) => {
