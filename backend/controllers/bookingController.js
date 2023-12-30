0401f3eda1b29c3bfc96519386f3ca449b638710
KaranGulve4342 <karangulve4342@gmail.com>
2023-12-30 17:39:00 +0530

    part-3 completed

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,6 +1,8 @@
 
 import Booking from '../models/Booking.js';
 
+// create new booking
+
 export const createBooking = async (req, res)=>{
 
     const newBooking = new Booking(req.body)
@@ -11,4 +13,41 @@ export const createBooking = async (req, res)=>{
     } catch (err) {
         res.status(500).json({success:true, message: "Internal server error"});
     }
-}
+};
+
+// get single booking
+
+export const getBooking = async(req, res)=>{
+    const id = req.params.id
+
+    try {
+        const book = await Booking.findById(id)
+
+        res.status(200)
+        .json({
+            success:true,
+            message: "successful",
+            data: book,
+        })
+    } catch (err) {
+        res.status(404).json({ success: true, message: "not found" });
+    }
+};
+
+// get All booking
+
+export const getAllBooking = async(req, res)=>{
+   
+    try {
+        const books = await Booking.findById(id)
+
+        res.status(200)
+        .json({
+            success:true,
+            message: "successful",
+            data: books,
+        })
+    } catch (err) {
+        res.status(500).json({ success: true, message: "internal server error" });
+    }
+};
